dad4b9fce81dcc5b29b89babe9e882ff72e1eecb
Altamash Ahmad <altamashahmad910@gmail.com>
2024-10-24 00:07:32 +0530

    Redux

---
+++ ToDo List Using ReduxToolkit/src/components/Todos.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+function Todos() {
+
+    const todos = useSelector(state=> state.todos)
+    const dispatch = useDispatch()
+
+  return (
+    <>
+        <div>Todos</div>
+        {todos.map((todo) => (
+            <li key={todo.id} className='list-none bg-blue-500 text-xl'> 
+                {todo.text}
+                <button className='mx-8 text-xl text-red-700 font-extrabold' onClick={()=>{dispatch(removeTodo(todo.id))}}>X</button>
+            </li>
+        ))}
+    </>
+  )
+}
+
+export default Todos
