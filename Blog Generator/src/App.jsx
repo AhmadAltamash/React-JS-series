ab8f3621206c738882cea4d844a8bd624e2bcf34
Altamash Ahmad <altamashahmad910@gmail.com>
2024-10-29 22:07:58 +0530

    blog

--- Blog Generator/src/App.jsx
+++ Blog Generator/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {Provider, useDispatch} from 'react-redux'
+import {Header, Footer} from './components/index.js'
 import './App.css'
+import authentication from './appwrite/auth.js'
+import login from './store/authSlice.js'
+import logout from './store/authSlice.js'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>Kaa ho</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  
+
+  useEffect(() => {
+    authentication.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+  
+  return !loading ? (
+      <div className="h-screen w-full flex flex-wrap content-between bg-gray-400">
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            {/* <Outlet/> */}
+            Todo:
+          </main>
+          <Footer/>
+        </div>
+      </div>
+  ) : null
 }
 
 export default App
