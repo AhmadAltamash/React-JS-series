81009beb550c60ac63ac8e3d0e030ba746e44d97
Altamash Ahmad <altamashahmad910@gmail.com>
2024-11-23 15:42:49 +0530

    commit

--- Blog Generator/src/pages/EditPost.jsx
+++ Blog Generator/src/pages/EditPost.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { Container, Post_Form } from '../components'
+import services from '../appwrite/conf'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function EditPost() {
-  return (
-    <div>EditPost</div>
-  )
+    const [post, setPost] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug){
+            services.getPost(slug)
+            .then((post)=>{
+                if(post){
+                    setPost(post)
+                }
+            })
+        }
+        else{
+            navigate('/')
+        }
+    },[slug,navigate])
+
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <Post_Form post={post} />
+        </Container>
+    </div>
+  ) : null
 }
 
 export default EditPost
