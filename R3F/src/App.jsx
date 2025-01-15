8219e0c2267a5f27fe5cda5d8436d481ef29daa7
Altamash Ahmad <altamashahmad910@gmail.com>
2025-01-15 23:39:20 +0530

    update

--- R3F/src/App.jsx
+++ R3F/src/App.jsx
@@ -1,14 +1,36 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { DirectionalLight } from 'three';
+import { useRef } from 'react';
+
+const RotatatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if(meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  })
+
+  return (
+    <mesh ref={meshRef} >
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color='#468585' emissive='#468585' />
+    </mesh>
+  )
+}
 
 const App = () => {
   return (
     <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <OrbitControls enableZoom={true} enablePan enableRotate />
+      <OrbitControls enableZoom enablePan enableRotate />
 
-      <DirectionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
+      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
       
+      <color attach='background' args={['#F0F0F0']}/>
+
+      <RotatatingCube/>
+
       </Canvas>
   )
 };
